caa5dd0b8dd8073a28fa194cc8cb04478d79d442
icaro paiva <icaropaivadbz.gmail.com>
2021-10-19 15:22:30 -0300

    modal quase todo funcional, falta o carrinho

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const c = el => document.querySelector(el)
 const cs = el => document.querySelectorAll(el)
 
-// Inicio do mapeamento de itens na tela
+let modalQt = 1
+
+// Listagem das pizzas
 pizzaJson.map((item, index) => {
   // Clonando o model do index.html
   let pizzaItem = c('.models .pizza-item').cloneNode(true)
@@ -20,19 +22,28 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector('a').addEventListener('click', e => {
     e.preventDefault()
 
-    // Recebendo o data-key de cada item anteriormente settado
+    // Recebendo o data-key de cada item anteriormente settado e quantidade escolhida no modal
     let key = e.target.closest('.pizza-item').getAttribute('data-key')
+    modalQt = 1
 
     // Preenchendo as informacoes do modal
     c('.pizzaBig img').src = pizzaJson[key].img
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
     c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
-    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
-      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
-    })
     c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(
       2
     )}`
+
+    // Resetando o tamanho escolhido dentro do modal
+    c('.pizzaInfo--size.selected').classList.remove('selected')
+    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add('selected')
+      }
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+    })
+
+    c('.pizzaInfo--qt').innerHTML = modalQt
 
     // Criando uma animacao de transparencia no modal
     c('.pizzaWindowArea').style.display = 'flex'
@@ -44,4 +55,38 @@ pizzaJson.map((item, index) => {
 
   // Exibir tudo que foi anteriormente settado
   c('.pizza-area').append(pizzaItem)
+})
+
+// Eventos do modal
+// Fecha o modal
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach(item => {
+  item.addEventListener('click', closeModal)
+})
+
+function closeModal() {
+  c('.pizzaWindowArea').style.opacity = 0
+  setTimeout(() => {
+    c('.pizzaWindowArea').style.display = 'none'
+  }, 500)
+}
+
+// Adiciona ou subtrai a quantidade de itens selecionados
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if (modalQt > 1) {
+    modalQt--
+    c('.pizzaInfo--qt').innerHTML = modalQt
+  }
+})
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+  modalQt++
+  c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+// Seleciona o tamanho
+cs('.pizzaInfo--size').forEach((size, indexSize) => {
+  size.addEventListener('click', e => {
+    console.log('clicou')
+    c('.pizzaInfo--size.selected').classList.remove('selected')
+    size.classList.add('selected')
+  })
 })
